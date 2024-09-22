cf74fb8ec23b23ceb20681c966a84e1e87a1d584
Alexander Vivas Jernström <alexanderjernstrom@gmail.com>
2024-09-22 09:33:19 +0200

    Rendered images on chess board

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,85 @@
-use std::{env, path, time::Duration};
+use std::{collections::HashMap, env, path, time::Duration};
 
+use conf::WindowMode;
+use dexterws_chess::game::{Board, Color as PieceColor, Piece, Square};
 use ggez::*;
 use glam::Vec2;
-use graphics::{Color, MeshBuilder};
+use graphics::{Color, DrawParam, MeshBuilder};
+use mint::Point2;
 
 struct State {
     //    image: graphics::Image,
     rect: graphics::Mesh,
+    board: Board,
+    piece_images: [Option<graphics::Image>; 64],
+}
+
+fn piece_to_image(piece: (Piece, PieceColor)) -> String {
+    let mut piece_image_map: HashMap<(Piece, PieceColor), String> = HashMap::new();
+    piece_image_map.insert(
+        (Piece::Bishop, PieceColor::White),
+        String::from("/white_bishop.png"),
+    );
+    piece_image_map.insert(
+        (Piece::Bishop, PieceColor::Black),
+        String::from("/black_bishop.png"),
+    );
+    piece_image_map.insert(
+        (Piece::Rook, PieceColor::White),
+        String::from("/white_rook.png"),
+    );
+    piece_image_map.insert(
+        (Piece::Rook, PieceColor::Black),
+        String::from("/black_rook.png"),
+    );
+    piece_image_map.insert(
+        (Piece::Pawn, PieceColor::White),
+        String::from("/white_pawn.png"),
+    );
+    piece_image_map.insert(
+        (Piece::Pawn, PieceColor::Black),
+        String::from("/black_pawn.png"),
+    );
+    piece_image_map.insert(
+        (Piece::Queen, PieceColor::White),
+        String::from("/white_queen.png"),
+    );
+    piece_image_map.insert(
+        (Piece::Queen, PieceColor::Black),
+        String::from("/black_queen.png"),
+    );
+    piece_image_map.insert(
+        (Piece::King, PieceColor::White),
+        String::from("/white_king.png"),
+    );
+    piece_image_map.insert(
+        (Piece::King, PieceColor::Black),
+        String::from("/black_king.png"),
+    );
+    piece_image_map.insert(
+        (Piece::Knight, PieceColor::White),
+        String::from("/white_knight.png"),
+    );
+    piece_image_map.insert(
+        (Piece::Knight, PieceColor::Black),
+        String::from("/black_knight.png"),
+    );
+
+    return piece_image_map.get(&piece).unwrap().clone();
 }
 
 fn draw_board(mb: &mut MeshBuilder) {
-    let white_square_color = graphics::Color::new(1.0, 1.0, 1.0, 1.0);
-    let black_square_color = graphics::Color::new(0.0, 0.0, 0.0, 1.0);
+    let white_square_color = graphics::Color::new(0.94, 0.85, 0.71, 1.0);
+    let black_square_color = graphics::Color::new(0.71, 0.53, 0.39, 1.0);
     for i in 0..8 {
         for j in 0..8 {
             mb.rectangle(
                 graphics::DrawMode::fill(),
                 graphics::Rect::new(
-                    100.0 + (j as f32 * 50.0),
-                    100.0 + (i as f32 * 50.0),
-                    50.0,
-                    50.0,
+                    100.0 + (j as f32 * 120.0),
+                    100.0 + (i as f32 * 120.0),
+                    120.0,
+                    120.0,
                 ),
                 if (i + j) % 2 == 1 {
                     black_square_color
@@ -36,18 +95,24 @@ fn draw_board(mb: &mut MeshBuilder) {
 impl State {
     fn new(ctx: &mut Context) -> GameResult<State> {
         //let image = graphics::Image::from_path(ctx, "/white_square.png")?;
+        let board = Board::new();
 
         let mb = &mut graphics::MeshBuilder::new();
-        mb.rectangle(
-            graphics::DrawMode::fill(),
-            graphics::Rect::new(450.0, 450.0, 50.0, 50.0),
-            graphics::Color::new(1.0, 1.0, 1.0, 1.0),
-        )?;
 
         draw_board(mb);
 
+        let pieces = board.get_all_pieces();
+        let piece_images: [Option<graphics::Image>; 64] = pieces.map(|piece| match piece {
+            Some(p) => Some(graphics::Image::from_path(ctx, piece_to_image(p)).unwrap()),
+            None => None,
+        });
+
         let rect = graphics::Mesh::from_data(ctx, mb.build());
-        let s = State { rect };
+        let s = State {
+            rect,
+            board,
+            piece_images,
+        };
 
         Ok(s)
     }
@@ -65,23 +130,27 @@ impl event::EventHandler<ggez::GameError> for State {
             graphics::Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
 
         // Draw an image.
-        let dst = glam::Vec2::new(20.0, 20.0);
         //canvas.draw(&self.image, graphics::DrawParam::new().dest(dst));
+
+        canvas.set_sampler(graphics::Sampler::nearest_clamp());
+        for (index, image) in self.piece_images.iter().enumerate() {
+            if image.is_some() {
+                let square = Square::from_idx(index as u8);
+                let x_pos: f32 = 160.0 + (120.0 * (square.file.to_idx() as f32));
+                let y_pos: f32 = 160.0 + (120.0 * (square.rank.to_idx() as f32));
+                let image_destination = glam::Vec2::new(x_pos, y_pos);
+                let piece_image = image.as_ref().unwrap();
+
+                canvas.draw(
+                    piece_image,
+                    graphics::DrawParam::new().dest(image_destination).z(100),
+                );
+            }
+        }
 
         // Draw an image with some options, and different filter modes.
 
-        canvas.set_sampler(graphics::Sampler::nearest_clamp());
         canvas.set_default_sampler();
-
-        // Draw a filled rectangle mesh.
-        /* let rect = graphics::Rect::new(450.0, 450.0, 50.0, 50.0);
-        canvas.draw(
-            &graphics::Quad,
-            graphics::DrawParam::new()
-                .dest(rect.point())
-                .scale(rect.size())
-                .color(Color::WHITE),
-        ); */
 
         // Draw a stroked rectangle mesh.
         canvas.draw(&self.rect, graphics::DrawParam::default());
@@ -103,7 +172,10 @@ fn main() -> GameResult {
     } else {
         path::PathBuf::from("./resources")
     };
-    let cb = ggez::ContextBuilder::new("drawing", "ggez").add_resource_path(resource_dir);
+    let window_mode = WindowMode::default().dimensions(1200.0, 1200.0);
+    let cb = ggez::ContextBuilder::new("drawing", "ggez")
+        .add_resource_path(resource_dir)
+        .window_mode(window_mode.resizable(true));
 
     let (mut ctx, events_loop) = cb.build()?;
 
